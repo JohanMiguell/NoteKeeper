c6e798b17110923fcf281011a76e57b8a03f5d42
JohanMiguel <jtojin-2020591@kinal.edu.gt>
2025-09-10 00:16:39 -0600

    CRUD user funcional con Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'ADMIN',
+    database: 'note_keeper',
+    autoLoadEntities: true,
+    synchronize: true,
+    }), 
+    UserModule
+  ],
 })
 export class AppModule {}
